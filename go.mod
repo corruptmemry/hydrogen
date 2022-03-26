06d5e47b320cbfbc9ffca0e0f19eef5b4fd22c35
corruptmemry <88046785+corruptmemry@users.noreply.github.com>
2022-03-26 21:30:47 +0700

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,10 @@
+module hydrogen/main
+
+go 1.16
+
+require (
+	github.com/BurntSushi/toml v1.0.0 // indirect
+	github.com/fhs/gompd v1.0.1 // indirect
+	github.com/hugolgst/rich-go v0.0.0-20210925091458-d59fb695d9c0 // indirect
+	github.com/marcusolsson/tui-go v0.4.0 // indirect
+)
---
+++ hydrogen.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"github.com/fhs/gompd/mpd"
+	"github.com/hugolgst/rich-go/client"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Port    string
+	Details string
+	AppID   string
+}
+
+var conf Config
+
+func readConfig() {
+	var home, _ = os.UserHomeDir()
+	if _, err := toml.DecodeFile(home+"/.config/hydrogenrpc/config.toml", &conf); err != nil {
+		os.Mkdir(home+"/.config/hydrogenrpc", 0755)
+		os.WriteFile(home+"/.config/hydrogenrpc/config.toml", []byte(`Details = "Hydrogen RPC"
+AppID = "857258957587087380"
+Port = "6670"`), 0755)
+		readConfig()
+	}
+}
+
+func main() {
+	readConfig()
+	fmt.Println("Details: " + conf.Details)
+	fmt.Println("Port: " + conf.Port)
+	fmt.Println("AppID: " + conf.AppID)
+	err := client.Login(conf.AppID)
+	if err != nil {
+		panic(err)
+	}
+	conn, err2 := mpd.Dial("tcp", "localhost:"+conf.Port)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	defer conn.Close()
+	for {
+		status, err3 := conn.Status()
+		if err3 != nil {
+			fmt.Println(err3)
+		}
+		song, err := conn.CurrentSong()
+		if err != nil {
+			fmt.Println(err3)
+		}
+		if status["state"] == "play" {
+			err = client.SetActivity(client.Activity{
+				State:   song["Title"],
+				Details: conf.Details,
+			})
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			time.Sleep(1e9)
+		}
+	}
+}
